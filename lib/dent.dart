3ad81fc39522162be854758c1452e88cb7e1ef97
Henrique C <hcortezi10@hotmail.com>
2023-06-14 18:54:02 -0300

    loc enviando

--- lib/dent.dart
+++ lib/dent.dart
@@ -14,6 +14,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
+
 class Dent extends StatelessWidget {
   const Dent({super.key});
 
